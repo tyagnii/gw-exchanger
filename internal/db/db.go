2767fc1cd7cde20910a8079bf49a4c26651fc089
tyagnii <aleksandr.tyagni@gmail.com>
2024-11-05 11:09:03 +0300

    Sketch a db connection

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,6 +6,7 @@ import (
 )
 
 type DBConnector interface {
+	InitSchema(ctx context.Context) error
 	SaveRates(ctx context.Context, rates models.Rates) error
 	GetRates(ctx context.Context) ([]models.Rates, error)
 	GetCurrencyRate(ctx context.Context, rate models.CurrencyRate) (models.CurrencyRateResponse, error)
