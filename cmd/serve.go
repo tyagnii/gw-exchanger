652c36891c68c4204057b985a959a172dcf1e49e
tyagnii <aleksandr.tyagni@gmail.com>
2024-11-06 16:29:16 +0300

    logger

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 	"github.com/tyagnii/gw-exchanger/config"
 	"github.com/tyagnii/gw-exchanger/gen/exchanger/v1"
 	"github.com/tyagnii/gw-exchanger/internal/db"
@@ -57,7 +56,6 @@ to quickly create a Cobra application.`,
 
 			DBconn, err = db.NewPGConnector(context.Background(), ConnString)
 			if err != nil {
-				fmt.Println(err)
 				sLogger.Errorf("Error connecting to database: %v", err)
 			} else {
 				break
